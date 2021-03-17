e34ce3cf13942b3ac43f86abb7017a4ee7c7971c
Chris Blättermann <chris.blaettermann@wirelane.com>
2021-03-17 12:34:16 +0100

    properly use pug template structure

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -2,16 +2,25 @@ import { Request, Response } from 'express';
 
 interface Hotel {
   title: string,
-  identifier: string
+  identifier: string,
+  termsAndConditionsUrl: string,
+  dataPrivacyUrl: string,
+  phone: string
 }
 const hotelConfiguration: { [key: string]: Hotel} = {
   "wirelane": {
     title: 'Wirelane',
-    identifier: 'wirelane'
+    identifier: 'wirelane',
+    termsAndConditionsUrl: 'https://www.wirelane.com/de/nutzungsbedingungen/',
+    dataPrivacyUrl: 'https://www.wirelane.com/datenschutzerklaerung/',
+    phone: '+49 800 399 499 599'
   },
   "maseven": {
     title: 'MASEVEN',
-    identifier: 'maseven'
+    identifier: 'maseven',
+    termsAndConditionsUrl: 'https://www.maseven.de/wp-content/uploads/2020/10/terms_of_conditions-1.pdf',
+    dataPrivacyUrl: 'https://www.maseven.de/en/privacy-policy/',
+    phone: '+49 89 998 29 44 0'
   }
 };
 
